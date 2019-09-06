3b806145a715e7d0d28fa6d0b7cd46f35f8c1abe
freethepickles <Aaronb317@gmail..com>
2019-09-05 22:58:56 -0500

    Added basic code for the options window I will implment the settings later

--- src/net/eternalconflict/www/Launcher.java
+++ src/net/eternalconflict/www/Launcher.java
@@ -27,13 +27,21 @@ public class Launcher {
     private JPasswordField passwordText;
     private JTextField usernameText;
     private JLabel serverstate;
+    public JCheckBox serverCheck1;
+    public JCheckBox serverCheck2;
+    public JCheckBox serverCheck3;
+    public JCheckBox serverCheck5;
+    public JCheckBox serverCheck10;
+    public JCheckBox checkNum1;
+    public JCheckBox checkNum2;
+    public JCheckBox checkNum3;
+    public JCheckBox checkNum5;
+    public JCheckBox checkNum10;
     public static Launcher instance;
     private List<DownloadHolder> filesNeeded;
     public Launcher() {
         instance = this;
-
-
-
+        
         filesNeeded = new ArrayList<DownloadHolder>();
         buttonListener = new ButtonListener();
 
@@ -77,7 +85,80 @@ public class Launcher {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JFrame options = new JFrame("Options and Settings");
-                JOptionPane.showMessageDialog(options, "This is a place holder for the opthions button.", "Options and settings ", JOptionPane.INFORMATION_MESSAGE);
+                options.setSize(900,700);
+                options.setResizable(false);
+                options.setBackground(Color.lightGray);
+                options.setVisible(true);
+                options.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                JPanel optionsPanel = new JPanel();
+
+                JLabel checkStat = new JLabel("Server checks status: ");
+
+                serverCheck1 = new JCheckBox("Every 1 minutes");
+                serverCheck2 = new JCheckBox("Every 2 minutes");
+                serverCheck3 = new JCheckBox("Every 3 minutes");
+                serverCheck5 = new JCheckBox("Every 5 minutes");
+                serverCheck10 = new JCheckBox("Every 10 mimutes");
+
+                JLabel checktime = new JLabel("Number of server checks per minute");
+
+                checkNum1 = new JCheckBox("1 Times");
+                checkNum2 = new JCheckBox("2 time");
+                checkNum3 = new JCheckBox("3 times");
+                checkNum5 = new JCheckBox("5 times");
+                checkNum10 = new JCheckBox("10 times");
+
+
+                GridBagConstraints optConstraints = new GridBagConstraints();
+
+                optConstraints.insets = new Insets(3, 3, 3, 3);
+
+                optConstraints.gridx = 1;
+                optConstraints.gridy = 1;
+                optionsPanel.add(checkStat,optConstraints);
+
+                optConstraints.gridx = 1;
+                optConstraints.gridy = 2;
+                optionsPanel.add(serverCheck1,optConstraints);
+                optConstraints.gridx = 2;
+                optConstraints.gridy = 2;
+                optionsPanel.add(serverCheck2,optConstraints);
+                optConstraints.gridx = 3;
+                optConstraints.gridy = 2;
+                optionsPanel.add(serverCheck3,optConstraints);
+                optConstraints.gridx = 4;
+                optConstraints.gridy = 2;
+                optionsPanel.add(serverCheck5,optConstraints);
+                optConstraints.gridx = 5;
+                optConstraints.gridy = 2;
+                optionsPanel.add(serverCheck10,optConstraints);
+
+                optConstraints.gridx = 2;
+                optConstraints.gridy = 2;
+                optionsPanel.add(checktime,optConstraints);
+
+                optConstraints.gridx = 2;
+                optConstraints.gridy = 3;
+                optionsPanel.add(checkNum1,optConstraints);
+
+                optConstraints.gridx = 3;
+                optConstraints.gridy = 3;
+                optionsPanel.add(checkNum2,optConstraints);
+
+                optConstraints.gridx = 4;
+                optConstraints.gridy = 3;
+                optionsPanel.add(checkNum3,optConstraints);
+
+                optConstraints.gridx = 5;
+                optConstraints.gridy = 3;
+                optionsPanel.add(checkNum5,optConstraints);
+
+                optConstraints.gridx = 6;
+                optConstraints.gridy = 3;
+                optionsPanel.add(checkNum10,optConstraints);
+                options.add(optionsPanel, BorderLayout.CENTER );
+                //options.pack();
             }
         });
         options.setMnemonic(KeyEvent.VK_O);
