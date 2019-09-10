7c913163002cbcd8fc88a7a2da0e1dbdf8c48353
freethepickles <Aaronb317@gmail..com>
2019-09-09 19:03:39 -0500

    Fixes a sentex error.

--- src/net/eternalconflict/www/enums/subtypes/PlanetTypeEnum.java
+++ src/net/eternalconflict/www/enums/subtypes/PlanetTypeEnum.java
@@ -6,8 +6,8 @@ public enum PlanetTypeEnum {
     OCEAN("Ocean", 2, 0.003f, 0.030f),
     HABITABLE("Habitable", 3, 0.006f, 0.009f),
     GAS("Gas", 4, 0.030f, 0.088f),
-    ICE("Ice", 5, 0.002f, 0.009f),
-    
+    ICE("Ice", 5, 0.002f, 0.009f),;
+
     private final float minSize;
     private final float maxSize;
     private final int id;
