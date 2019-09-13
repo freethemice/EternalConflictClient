9f0e14a23f53cca6ce92bd9d194525b76025f0fc
Daniel Appleby <cgfreethemice@gmail.com>
2019-09-13 18:07:17 -0500

    changes url to correct file

--- src/net/eternalconflict/www/enums/ServerInfoEnum.java
+++ src/net/eternalconflict/www/enums/ServerInfoEnum.java
@@ -5,7 +5,7 @@ public enum ServerInfoEnum {
     LOGIN("Login", "51.79.120.233"),
     WEB("Web", "http://www.firesoftitan.com/ec"),
     REGISTER("Register", "http://www.firesoftitan.com/ec"),
-    VERSIONS("Versions", "http://www.firesoftitan.com/ec/info.html"),
+    VERSIONS("Versions", "http://www.firesoftitan.com/ec/info.data"),
     ISSUES("Issues", "https://github.com/freethemice/EternalConflictClient/issues");
     private final String name;
     private final String address;
